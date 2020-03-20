060fd850b4bb962f8b4cb342d9a6f714763f639f
Alexey Bubnov <bubnov.alexey@gmail.com>
2020-03-21 02:44:36 +0300

    Finish

---
+++ src/BracketsMathematicalExpressions.php
@@ -0,0 +1,44 @@
+<?php
+
+
+namespace AlexVenga;
+
+//use InvalidArgumentException;
+
+use InvalidArgumentException;
+
+class BracketsMathematicalExpressions
+{
+
+    protected $mathematicalExpression;
+
+    public function __construct($mathematicalExpression)
+    {
+
+        if (!$this->checkMathematicalExpression()) {
+            throw new InvalidArgumentException('The equation contains invalid characters');
+        }
+
+        $this->mathematicalExpression = $mathematicalExpression;
+    }
+
+    protected function checkMathematicalExpression()
+    {
+        if (preg_match('/[^() \t\n\r]/', $this->mathematicalExpression)) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    public function getMathematicalExpression()
+    {
+        return $this->mathematicalExpression;
+    }
+
+    public function getClearMathematicalExpression()
+    {
+        return preg_replace('/[ \t\n\r]+/', '', $this->mathematicalExpression);
+    }
+
+}
---
+++ src/CheckBrackets.php
@@ -0,0 +1,26 @@
+<?php
+
+
+namespace AlexVenga;
+
+class CheckBrackets
+{
+
+    public function checkBracketsInString(BracketsMathematicalExpressions $stringClass)
+    {
+
+        $string = $stringClass->getClearMathematicalExpression();
+
+        while (preg_match('/\(\)/', $string)) {
+            $string = preg_replace('/\(\)/', '', $string);
+        };
+
+        if (empty($string)) {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+
+}
